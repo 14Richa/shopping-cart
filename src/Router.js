cc886cbcf3560ba80e481826c86b7567cdb095de
Richa Sharma <onlinericha19@gmail.com>
2018-10-22 23:38:38 +0530

    first commit

---
+++ src/Router.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import { Switch, Route } from 'react-router-dom'
+
+import HomePage from './pages/Homepage'
+import CartPage from './pages/Cartpage'
+
+const Router = () => (
+   <Switch>
+     <Route exact path = '/' component = { HomePage } />
+     <Route exact path = '/cart' component = { CartPage } />    
+     </Switch>
+	)
+	
+
+export default Router
